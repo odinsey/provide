9d028d99b8a2faf7463c960e72c2aa9b1612a8a2
Francis Hilaire <prometee@gmail.com>
2013-03-11 03:30:27 +0100

    Bug si aucun champ n'est mis à jour le fichier n'est pas mis à jour

--- src/NP/Bundle/EventBundle/Form/Handler/EventFormHandler.php
+++ src/NP/Bundle/EventBundle/Form/Handler/EventFormHandler.php
@@ -3,8 +3,13 @@
 namespace NP\Bundle\EventBundle\Form\Handler;
 
 use NP\Bundle\CoreBundle\Form\Handler\BaseFormHandler;
+use Symfony\Component\Form\Form;
 
 class EventFormHandler extends BaseFormHandler {
     protected $class_name = 'NP\Bundle\EventBundle\Entity\Event';
+    
+    protected function preSave(Form $form, $entity, $controller) {
+        $entity->setUpdatedAt(new \Datetime());
+    }
 
 }
